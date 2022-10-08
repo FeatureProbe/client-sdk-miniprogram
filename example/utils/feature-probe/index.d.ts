9e2e4b9cc27f00e14a1725679e2e64fb2d97de8c
panchongyang <yourhand1025@163.com>
2022-10-08 18:13:12 +0800

    perf(example): added package.json
    added package.json

--- example/utils/feature-probe/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { FPUser } from './FPUser';
-import { FPToggleDetail, featureProbeClient } from './FeatureProbe';
-export { FPUser, FPToggleDetail, featureProbeClient };
-declare global {
-    let App: (config?: any) => any;
-    let getApp: () => any;
-}
